6c658ff9f9bfbe31b800fc4b14067d1d287ff79c
regitabish <shiba.taiga.lab@gmail.com>
2024-08-23 22:17:20 +0900

    https://docs.google.com/presentation/d/1CIMDenDLZ7NPNgzmfbCNH_W3dYjaTEBdUYfUuXXuMHk/edit#slide=id.g4e29972148_0_169

--- test.go
+++ test.go
@@ -1,5 +1,26 @@
 package main
 
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
 func main() {
 	println("hello world!")
+	var str string = "hello world from variable"
+	println(str)
+
+	/* for i := 0; i <= 100; i++ {
+		if i%2 == 0 {
+			println(i, "偶数")
+		} else {
+			println(i, "奇数")
+		}
+	} */
+
+	t := time.Now().UnixNano()
+	rand.Seed(t)
+	n := rand.Intn(6)
+	fmt.Println(n)
 }
